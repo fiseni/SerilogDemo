f22b3b877c6c280fe6f33b7aec08a90472667b99
Fati Iseni <fati.iseni@pozitrongroup.com>
2024-04-22 21:36:17 +0200

    Added test endpoints.

---
+++ src/SerilogDemo.Web/TestController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SerilogDemo.Web.Data;
+
+namespace SerilogDemo.Web;
+
+[ApiController]
+public class TestController : ControllerBase
+{
+    private readonly ILogger<TestController> _logger;
+
+    public TestController(ILogger<TestController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("test")]
+    public IActionResult Test()
+    {
+        _logger.LogInformation("Test method called");
+
+        return Ok();
+    }
+
+    [HttpGet("test-error")]
+    public IActionResult TestError()
+    {
+        try
+        {
+            var x = 0;
+            _ = 1 / x;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Test error called");
+        }
+
+        return Ok();
+    }
+
+    [HttpGet("test-db")]
+    public async Task<IActionResult> TestDb([FromServices] AppDbContext dbContext, CancellationToken cancellationToken)
+    {
+        // Testing EF log outputs.
+
+        var customers = await dbContext.Customers.ToListAsync(cancellationToken);
+
+        return Ok(customers);
+    }
+}
