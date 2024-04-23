c4913898a4aa923a5e0d658f863cc958d720d05c
Fati Iseni <fati.iseni@pozitrongroup.com>
2024-04-23 11:27:34 +0200

    Added CurrentUser implementation.

---
+++ src/SerilogDemo.Web/CurrentUser.cs
@@ -0,0 +1,70 @@
+﻿using Serilog.Context;
+using System.Security.Claims;
+
+namespace SerilogDemo.Web;
+
+// Refer to this article for CurrentUser implementation.
+// https://fiseni.com/posts/current-user-aspnetcore/
+public class CurrentUser : ICurrentUser, ICurrentUserInitializer
+{
+    public string? UserId { get; set; }
+    public string? UserName { get; set; }
+}
+
+public interface ICurrentUser
+{
+    string? UserId { get; }
+    string? UserName { get; }
+}
+
+public interface ICurrentUserInitializer
+{
+    string? UserId { get; set; }
+    string? UserName { get; set; }
+}
+
+public static class CurrentUserExtensions
+{
+    public static IServiceCollection AddCurrentUser(this IServiceCollection services)
+    {
+        services.AddScoped<CurrentUser>();
+        services.AddScoped<ICurrentUser, CurrentUser>(x => x.GetRequiredService<CurrentUser>());
+        services.AddScoped<ICurrentUserInitializer, CurrentUser>(x => x.GetRequiredService<CurrentUser>());
+
+        return services;
+    }
+
+    public static IApplicationBuilder UseCurrentUserFromToken(this IApplicationBuilder app, bool pushToSerilogContext = true)
+    {
+        app.Use(async (context, next) =>
+        {
+            var currentUser = context.RequestServices.GetService<ICurrentUserInitializer>();
+
+            if (currentUser is not null)
+            {
+                //currentUser.UserId = context.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+                //currentUser.UserName = context.User?.FindFirst(ClaimTypes.Name)?.Value;
+
+                // For the demo we'll just set some values.
+                currentUser.UserId = "1";
+                currentUser.UserName = "admin";
+            }
+
+            if (pushToSerilogContext)
+            {
+                using (LogContext.PushProperty(nameof(ICurrentUserInitializer.UserId), currentUser?.UserId))
+                using (LogContext.PushProperty(nameof(ICurrentUserInitializer.UserName), currentUser?.UserName))
+                {
+                    await next();
+                }
+            }
+            else
+            {
+                await next();
+            }
+
+        });
+
+        return app;
+    }
+}
--- src/SerilogDemo.Web/Program.cs
+++ src/SerilogDemo.Web/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Serilog;
+using SerilogDemo.Web;
 using SerilogDemo.Web.Data;
 
 // If you need to log or catch exceptions during the build phase and app startup,
@@ -8,11 +9,12 @@ using SerilogDemo.Web.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.ConfigureSerilog();
+builder.AddSerilog();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
 
+builder.Services.AddCurrentUser();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -23,19 +25,21 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.UseSerilogRequestLogging();
 app.UseHttpsRedirection();
 app.UseAuthorization();
+app.UseCurrentUserFromToken();
+app.UseSerilogRequestLogging();
 app.MapControllers();
 
 app.Run();
 
 
+// Added these extensions just to demonstrate different ways of configuring Serilog.
 public static class LoggingExtensions
 {
     // For ASP.NET Core apps, moving forward this is the canonical way to configure Serilog. Do not use builder.Host.UseSerilog().
     // This will set up Serilog as the only logging provider, it replaces the default logging provider.
-    public static WebApplicationBuilder ConfigureSerilog(this WebApplicationBuilder builder)
+    public static WebApplicationBuilder AddSerilog(this WebApplicationBuilder builder)
     {
         builder.Services.AddSerilog((services, lc) => lc
             .ReadFrom.Configuration(builder.Configuration)
@@ -45,7 +49,7 @@ public static class LoggingExtensions
     }
 
     // You may do it this way as well. Refer to Two-stage initialization for a better version of this.
-    public static WebApplicationBuilder ConfigureSerilog2(this WebApplicationBuilder builder)
+    public static WebApplicationBuilder AddSerilog2(this WebApplicationBuilder builder)
     {
         Log.Logger = new LoggerConfiguration()
             .ReadFrom.Configuration(builder.Configuration)
@@ -57,16 +61,15 @@ public static class LoggingExtensions
     }
 
     // This adds the Serilog just as a provider to the existing logging infrastructure.
-    public static WebApplicationBuilder ConfigureSerilog3(this WebApplicationBuilder builder)
+    public static WebApplicationBuilder AddSerilog3(this WebApplicationBuilder builder)
     {
-        var logger = new LoggerConfiguration()
+        Log.Logger = new LoggerConfiguration()
             .ReadFrom.Configuration(builder.Configuration)
             .CreateLogger();
 
         builder.Logging.ClearProviders();
-        builder.Logging.AddSerilog(logger);
+        builder.Logging.AddSerilog();
 
         return builder;
     }
-
 }
